fa6593b16c0b841aaf84d45c5b34a341dc6b6c27
amenaijp <amenaijp@gmail.com>
2026-05-12 15:41:40 +1200

    feat: menu button toggles to x

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import './layout.css';
 	import { resolve } from '$app/paths';
 
+	let menu_open = $state(false);
+
 	let { children } = $props();
 </script>
 
@@ -11,7 +13,7 @@
 		<!-- First column; hamburger menu -->
 		<div class="flex items-center">
 			<!-- Hamburger menu icon -->
-			<button class="flex hover:cursor-pointer" aria-label="Open Menu">
+			<button class="flex hover:cursor-pointer" aria-label="Open Menu" onclick={() => menu_open = !menu_open}>
 				<svg
 					class="ml-4 size-10 md:size-11 lg:size-14"
 					fill="none"
@@ -20,11 +22,17 @@
 					viewBox="0 0 24 24"
 					xmlns="http://www.w3.org/2000/svg"
 				>
-					<path
-						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					/>
+					{#if !menu_open}
+						<!-- Menu icon (from hero icons)  -->
+						<path
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+					{:else}
+						<!-- x-mark icon  -->
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+					{/if}
 				</svg>
 			</button>
 		</div>
