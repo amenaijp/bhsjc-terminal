0c4dbdc752909b4996e888874ec90f11ca3a8661
amenaijp <amenaijp@gmail.com>
2026-05-12 09:24:49 +0900

    chore: Delete .svelte-kit directory

--- .svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/article/[article_id]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/article/[article_id]/+page.svelte";
--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/genre/[genre]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/genre/[genre]/+page.svelte";
