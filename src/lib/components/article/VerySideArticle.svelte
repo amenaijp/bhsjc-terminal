f1a57a478c71c8b4f5f2febf45a435b096f29a90
amenaijp <amenaijp@gmail.com>
2026-05-15 12:37:47 +1200

    chore: move specific html from front page to VerySideArticle.svelte

---
+++ src/lib/components/article/VerySideArticle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Article from '$lib/components/article/Article.svelte';
+	import type { Article as ArticleType } from '$lib/server/db/schema';
+	import { resolve } from '$app/paths';
+
+	interface Props {
+		article: ArticleType;
+	}
+
+	let { article }: Props = $props();
+</script>
+
+<Article link={resolve(`/article/${article.id}`)} class="self-center">
+	<h4 class="font-[Playfair] text-2xl wrap-anywhere group-hover:underline lg:text-3xl">
+		{article.title}
+	</h4>
+</Article>
