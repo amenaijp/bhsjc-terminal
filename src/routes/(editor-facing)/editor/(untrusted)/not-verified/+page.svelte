709148bfd747361c2e277a4469188390038c4f17
amenaijp <amenaijp@gmail.com>
2026-05-16 17:02:17 +1200

    feat: not-verified/+page.svelte is done

--- src/routes/(editor-facing)/editor/(untrusted)/not-verified/+page.svelte
+++ src/routes/(editor-facing)/editor/(untrusted)/not-verified/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-
+	import { resolve } from "$app/paths"
 </script>
 
-<p>haha you aren't verified (dumbass)</p>
+<div class="flex flex-col m-2">
+	<p>
+		You've successfully made your Terminal account! Make sure a club admin knows that you're waiting to be verified, then sit tight.
+		<br />
+		In the meantime, have you verified your email address yet? We may assume you're a bot or malicious actor if you haven't.
+	</p>
+
+	<a href={resolve("/")} class="text-blue-600 hover:text-blue-600 hover:underline mx-2 w-fit">return to homepage</a>
+</div>
