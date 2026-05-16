340a747e0dd0961c6183e147fc716158d1564590
amenaijp <amenaijp@gmail.com>
2026-05-16 19:44:02 +1200

    fix: just a little more typing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,13 +2,14 @@ import type { Handle } from '@sveltejs/kit';
 import { building } from '$app/environment';
 import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+import type { User } from '$lib/server/auth';
 
 const handleBetterAuth: Handle = async ({ event, resolve }) => {
 	const session = await auth.api.getSession({ headers: event.request.headers });
 
 	if (session) {
 		event.locals.session = session.session;
-		event.locals.user = session.user as App.Locals['user'];
+		event.locals.user = session.user as User;
 	}
 
 	return svelteKitHandler({ event, resolve, auth, building });
