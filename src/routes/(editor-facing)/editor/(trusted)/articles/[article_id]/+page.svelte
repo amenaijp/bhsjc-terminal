4328af2eed0a06586c665e25fa9ef6c9266114a6
amenaijp <amenaijp@gmail.com>
2026-05-21 15:57:15 +1200

    fix: enter on title input would trigger image upload dialog

--- src/routes/(editor-facing)/editor/(trusted)/articles/[article_id]/+page.svelte
+++ src/routes/(editor-facing)/editor/(trusted)/articles/[article_id]/+page.svelte
@@ -305,7 +305,7 @@
 				}}
 			/>
 			<button
-				class="m-2 w-fit rounded-sm bg-blue-600 px-2 py-1 text-white transition hover:bg-blue-700"
+				class="m-2 w-fit rounded-sm bg-blue-600 px-2 py-1 text-white transition hover:bg-blue-700" type="button"
 				onclick={() => uploadImageElement?.click()}
 			>
 				{frontImage ? 'Change image...' : 'Upload an image...'}
