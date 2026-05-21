2089b2e687c83b55721af8f616d082ff6c9511b9
amenaijp <amenaijp@gmail.com>
2026-05-21 23:05:30 +1200

    feat: pages load from db as intended

--- src/routes/(user-facing)/article/[article_id]/+page.server.ts
+++ src/routes/(user-facing)/article/[article_id]/+page.server.ts
@@ -1,5 +1,28 @@
-export async function load({ params }) {
+import { error } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import { article, articleAuthor, articleGenre, user } from '$lib/server/db/schema';
+import { db } from '$lib/server/db';
+
+export async function load({ params, locals }) {
 	const { article_id } = params;
 
-	return { article_id };
+	const viewing_article = db.select().from(article).where(eq(article.id, article_id)).get();
+	if (!viewing_article || !viewing_article.published) return error(404);
+
+	const authors = await db
+		.select({ name: user.name })
+		.from(articleAuthor)
+		.innerJoin(user, eq(articleAuthor.editorId, user.id))
+		.where(eq(articleAuthor.articleId, article_id));
+
+	const genres = await db
+		.select({ genre: articleGenre.genre })
+		.from(articleGenre)
+		.where(eq(articleGenre.articleId, article_id));
+
+	return {
+		article: viewing_article,
+		authors,
+		genres
+	};
 }
--- src/routes/(user-facing)/article/[article_id]/+page.svelte
+++ src/routes/(user-facing)/article/[article_id]/+page.svelte
@@ -1,95 +1,64 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { formatDistanceToNow } from 'date-fns';
+	import { resolve } from '$app/paths';
 
 	let { data }: { data: PageData } = $props();
 </script>
 
 <svelte:head>
-	<title>{data.article_id}</title>
+	<title>{data.article.title}</title>
 </svelte:head>
-
 <!-- Divider -->
 <div class="my-4 flex h-0.5 rounded-xs bg-[#132d23]"></div>
 
-<!-- top articles -->
-<article class="mt-4 flex max-w-[1100px] flex-col self-center md:px-3">
+<article class="mt-4 mb-4 flex max-w-275 flex-col self-center md:px-3">
 	<!-- Title, Date, Journalists -->
-	<header class="flex flex-col self-center px-3 md:max-w-[600px] lg:max-w-[800px]">
+	<header class="mb-4 flex flex-col self-center px-3 md:max-w-150 lg:max-w-200">
 		<h2
 			class="self-center text-left font-[Playfair] text-4xl wrap-anywhere md:text-5xl lg:text-6xl"
 		>
-			a VERY interesting article title
+			{data.article.title}
 		</h2>
 		<div class="flex flex-row">
-			<p class="md:text-md mt-3 self-center text-left text-sm text-[#666666]">67 days ago</p>
+			<p class="md:text-md mt-3 self-center text-left text-sm text-[#666666]">
+				last updated {data.article.updatedAt.toLocaleDateString('en-NZ')}, {formatDistanceToNow(
+				data.article.updatedAt
+			)} ago
+			</p>
 			<div class="flex flex-1"></div>
-			<p class="md:text-md mt-3 self-center text-left text-sm">
-				<span class="text-[#666666]">Genre:</span>
-				<a class="hover:underline" href="#">Bikini Bottom</a>
+			<p class="md:text-md mt-3 self-center text-left text-sm text-[#666666]">
+				Genre{data.genres.length > 1 ? 's' : ''}:
+				{#if data.genres.length > 1}
+					{#each data.genres as genre, i}
+						<a class="text-black hover:cursor-pointer hover:underline" href={resolve(`/genre/${genre.genre}`)}
+						>{genre.genre.charAt(0).toUpperCase() + genre.genre.slice(1)}</a
+						>{#if i < data.genres.length - 1}{', '}{/if}
+					{/each}
+				{:else}
+					none
+				{/if}
 			</p>
 		</div>
 		<p class="md:text-md mt-3 text-left text-sm wrap-anywhere">
-			by <b class="text-md text-[#132d23] md:text-lg">Squidward Tentacles</b> and
-			<b class="text-md wrap-anywhere text-[#132d23] md:text-lg">Spongebob SquarePants</b>
+			by {#each data.authors as author, i}
+			<b class="text-md text-[#132d23] md:text-lg"
+			>{author.name}</b
+			>{#if i < data.authors.length - 2},
+		{:else if i < data.authors.length - 1}{' and '}{/if}
+		{/each}
 		</p>
 	</header>
-	<img
-		alt="Article Image"
-		class="mt-10 flex aspect-video self-center md:rounded-xs md:px-5"
-		src="https://picsum.photos/1920/1080"
-	/>
-	<!-- Actual article content: needs to be treated as dangerous and since it's being parsed through a markdown->html converter it will have slightly different html semantics compared to the rest of the file-->
-	<div class="flex flex-col self-center px-3 md:max-w-[600px] lg:max-w-[800px]">
+	{#if data.article.frontImage}
+		<!-- TODO: one day we should start saving the alt info -->
+		<div class="mt-6 mb-4 aspect-video w-full self-center overflow-hidden md:rounded-xs md:px-5">
+			<img alt="" class="h-full w-full object-cover" src={data.article.frontImage} />
+		</div>
+	{/if}
+	<div class="flex flex-row self-center px-3 md:w-150 lg:w-200">
 		<!-- whitespace-pre-wrap is just to get \n\n to render while using lorem ipsum, remove when using markdown-->
 		<p class="text-left font-[Playfair] text-lg wrap-anywhere whitespace-pre-line md:text-xl">
-			Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
-			pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
-			sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis
-			massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
-			sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet
-			consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id
-			cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
-			vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia
-			integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
-			per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit.
-			Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus
-			duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus
-			nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
-			hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
-			inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus
-			ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.
-			Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
-			egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel
-			class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem
-			ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
-			pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
-			sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis
-			massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
-			sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet
-			consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id
-			cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
-			vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia
-			integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
-			per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit.
-			Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus
-			duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus
-			nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
-			hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
-			inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus
-			ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.
-			Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
-			egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel
-			class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem
-			ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
-			pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
-			sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis
-			massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
-			sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet
-			consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id
-			cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
-			vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia
-			integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
-			per conubia nostra inceptos himenaeos.
+			{data.article.fullText}
 		</p>
 	</div>
 </article>
