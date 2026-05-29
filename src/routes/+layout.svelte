4064f97d4050134edc94059b8a32e61dfe817cd9
amenaijp <amenaijp@gmail.com>
2026-05-30 03:00:27 +1200

    feat: add umami analytics to the dev environment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import './layout.css';
+	import { env } from '$env/dynamic/public';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<script defer src="/umami.js" data-website-id={env.PUBLIC_UMAMI_APP_ID}></script>
+</svelte:head>
 
 {@render children()}
